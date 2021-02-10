b30b92e450809228fa6f6c9455c30df97be9a9ea
Ramkumar More <ramkumarmore@gmail.com>
2021-02-10 23:03:04 +0530

    Imported ui-library module inside admin-ui project

--- admin-ui/src/app/app.module.ts
+++ admin-ui/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// UI LIBRARY MODULE
+import { UiLibraryModule } from 'ui-library';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
